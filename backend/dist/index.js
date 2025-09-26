ec96d3bd1d0292aee4f8351be5d2cfceb5e8c8a0
ElCarambola <yakovchikdenis@gmail.com>
2025-09-26 15:47:14 +0300

    fix: add dist folder

---
+++ backend/dist/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import todosRouter from './todos/router.js';
+import { client } from './shared/api/mongodb-client.js';
+const PORT = process.env.PORT;
+const app = express();
+const corsOptions = {
+    origin: 'http://localhost:8081',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/api', todosRouter);
+async function main() {
+    try {
+        await client.connect();
+        app.listen(PORT, () => console.log('SERVER LISTEN PORT' + PORT));
+    }
+    catch (e) {
+        await client.close();
+        console.log(e);
+    }
+}
+main();
+//# sourceMappingURL=index.js.map
